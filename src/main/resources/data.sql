bf7631c0d4b0d7e83f6749ec37322fc869da1b83
Douglas Monteiro Cavalcanti <douglas.cavalcanti@ebanx.com>
2025-02-06 15:01:51 -0300

    add more data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,11 @@
 INSERT INTO customer (id, active, email) VALUES (1, true, 'joao@test.com');
 INSERT INTO customer (id, active, email) VALUES (2, true, 'maria@test.com');
+INSERT INTO customer (id, active, email) VALUES (3, false, 'maria@test.com');
 
-INSERT INTO product (id, code, price) VALUES (10, 'SWITCH', 1899.0);
+INSERT INTO product (id, code, price) VALUES (10, 'NINTENDO-SWITCH', 1899.0);
 INSERT INTO product (id, code, price) VALUES (20, 'PS5', 3299.0);
 INSERT INTO product (id, code, price) VALUES (30, 'XBOX-ONE', 2499.0);
 
 INSERT INTO purchase (id, customer_id, product_id) VALUES (100, 1, 10);
 INSERT INTO purchase (id, customer_id, product_id) VALUES (200, 2, 20);
+INSERT INTO purchase (id, customer_id, product_id) VALUES (300, 3, 20);
